569a2f5cfacda1a45d125fd3091dc0790b603f8a
dothdat <dodat211004@gmail.com>
2024-03-30 01:59:11 +0700

    hoa1.5
    xong phần hoa tự động và va chạm nhưng chưa xét được animation lúc chết cho mario

--- Assets/Scripts/HoaAnThit.cs
+++ Assets/Scripts/HoaAnThit.cs
@@ -1,35 +1,51 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HoaAnThit : MonoBehaviour
 {
-   public GameObject objectToShow;
-    public float hideDelay = 5f;
-    public float showDelay = 3f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-       StartCoroutine(HideAndShow());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-    private IEnumerator HideAndShow()
+    public float thoiGianNgoiLen = 5f;
+    public float thoiGianOTrong = 3f;
+    public float thoiGianChuiXuong = 5f;
+    public float chieuCaoToiDa = 10f;
+    public float chieuCaoToiThieu = 0f;
+    
+    
+    private IEnumerator Start()
     {
         while (true)
         {
-            // Ẩn GameObject trong 5 giây
-            objectToShow.SetActive(false);
-            yield return new WaitForSeconds(hideDelay);
-
-            // Hiện GameObject trong 3 giây
-            objectToShow.SetActive(true);
-            yield return new WaitForSeconds(showDelay);
+            yield return LerpPosition(new Vector3(transform.position.x, chieuCaoToiDa, transform.position.z), thoiGianNgoiLen);
+            yield return new WaitForSeconds(thoiGianOTrong);
+            yield return LerpPosition(new Vector3(transform.position.x, chieuCaoToiThieu, transform.position.z), thoiGianChuiXuong);
         }
     }
+
+    private IEnumerator LerpPosition(Vector3 viTriMucTieu, float thoiGian)
+    {
+        Vector3 viTriBanDau = transform.position;
+        float thoiGianDaTieu = 0f;
+
+        while (thoiGianDaTieu < thoiGian)
+        {
+            float tiLe = thoiGianDaTieu / thoiGian;
+            transform.position = Vector3.Lerp(viTriBanDau, viTriMucTieu, tiLe);
+            thoiGianDaTieu += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = viTriMucTieu;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+            GameManager.Instance.ResetLevel(3f);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    
 }
